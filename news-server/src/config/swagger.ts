8eebfba0da63991b088cb2edaff69e2c90c9a2dd
donghyun0118 <kalim0118@rs-team.com>
2025-10-20 11:11:06 +0900

    chore(debug): Add admin.ts to Swagger build for testing

--- news-server/src/config/swagger.ts
+++ news-server/src/config/swagger.ts
@@ -3,7 +3,7 @@ import { swaggerDefinition } from "./swagger-definition";
 
 const options: swaggerJsdoc.Options = {
   definition: swaggerDefinition,
-  apis: ["./src/routes/**/*.ts"],
+  apis: ["./src/routes/jobs.ts", "./src/routes/admin.ts"],
 };
 
 export const specs = swaggerJsdoc(options);
