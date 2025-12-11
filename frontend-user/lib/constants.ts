7cfd956fa31f57a4380724faaf783beaf6f4ab0e
kalim200123 <thsehdgus1234@gmail.com>
2025-12-11 11:05:03 +0900

    fix: use env variable for backend url

--- frontend-user/lib/constants.ts
+++ frontend-user/lib/constants.ts
@@ -1,4 +1,4 @@
-export const BACKEND_BASE_URL = "http://localhost:3001";
+export const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
 
 // A map of known favicon URLs for specific news sources.
 // Can be populated over time.
